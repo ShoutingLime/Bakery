87d3e90e62cad7a943ab69a560893d0095d0fb34
ShoutingLime <shouting.lime@gmail.com>
2017-01-23 23:44:27 +0300

    Commit 6
    Almost correct shutter.

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,8 @@
-/*global window, document, getElementsByClassName, moveAt, $*/
+/*global window, document, getElementsByClassName, moveAt, $, console, screen, windowWidth, self*/
 
-var separator = document.getElementById('separator'),
+var html = document.documentElement,
+  body = document.body,
+  separator = document.getElementById('separator'),
   showcasePartRight = document.getElementById('showcasePartRight'),
   showcasePartLeft = document.getElementById('showcasePartLeft'),
   showcasePartRightVisible = document.getElementById('showcasePartRightVisible');
@@ -19,11 +21,56 @@ function getCoords(elem) {
   return { top: Math.round(top), left: Math.round(left) };
 }
 
+function startPosition() {
+  'use strict';
+  if (html.clientWidth === 1903) { // 100%
+    separator.style.left = 812 + 'px';
+    showcasePartRight.style.left = 837 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 2114) { // 90%
+    separator.style.left = 916 + 'px';
+    showcasePartRight.style.left = 941 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 2379) { // 80%
+    separator.style.left = 1048 + 'px';
+    showcasePartRight.style.left = 1073 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 2537) { // 75%
+    separator.style.left = 1128 + 'px';
+    showcasePartRight.style.left = 1153 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 2855) { // 67%
+    separator.style.left = 1287 + 'px';
+    showcasePartRight.style.left = 1312 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 3806) { // 50%
+    separator.style.left = 1764 + 'px';
+    showcasePartRight.style.left = 1789 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 5709) { // 33%
+    separator.style.left = 2715 + 'px';
+    showcasePartRight.style.left = 2740 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 7612) { // 25%
+    separator.style.left = 3668 + 'px';
+    showcasePartRight.style.left = 3693 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 1730) { // 110%
+    separator.style.left = 725 + 'px';
+    showcasePartRight.style.left = 750 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  } else if (html.clientWidth === 1522 || html.clientWidth === 1269 || html.clientWidth === 1087 || html.clientWidth === 952 || html.clientWidth === 761 || html.clientWidth === 634 || html.clientWidth === 476 || html.clientWidth === 381) { // 125%, 150%, 175%, 200%, 250%, 300%, 400%, 500%
+    separator.style.left = 660 + 'px';
+    showcasePartRight.style.left = 685 + 'px';
+    showcasePartRight.style.width = 430 + 'px';
+  }
+}
+
 separator.onmousedown = function (e) {
   'use strict';
-  var coords = getCoords(separator),
-    shiftX = e.pageX - coords.left - 25; // сдвиг по X относительно центра сепаратора
   separator.style.position = 'absolute';
+  var coords = getCoords(separator),
+    shiftX = e.pageX - coords.left - 25;
   document.body.appendChild(separator);
   moveAt(e);
   showcasePartRightVisible.style.zIndex = 1000;
@@ -32,10 +79,19 @@ separator.onmousedown = function (e) {
   showcasePartRight.style.zIndex = 900;
 
   function moveAt(e) {
-    if (e.pageX > 837 && e.pageX < 1000) {
-      separator.style.left = e.pageX - 25 + 'px';
-      showcasePartRight.style.left = e.pageX + 'px';
-      showcasePartRight.style.width = 965 - (e.pageX - shiftX - 350) + 'px';
+    var field = (html.clientWidth - 1600) / 2;
+    if (field > 0) {
+      if (e.pageX > (field + 685 + shiftX) && e.pageX < (field + 835 + shiftX)) {
+        separator.style.left = e.pageX - shiftX - 25 + 'px';
+        showcasePartRight.style.left = e.pageX - shiftX + 'px';
+        showcasePartRight.style.width = 250 + 'px';
+      }
+    } else {
+      if (e.pageX > 685 + shiftX && e.pageX < 835 + shiftX) {
+        separator.style.left = e.pageX - shiftX - 25 + 'px';
+        showcasePartRight.style.left = e.pageX - shiftX + 'px';
+        showcasePartRight.style.width = 250 + 'px';
+      }
     }
   }
 
@@ -53,6 +109,8 @@ separator.ondragstart = function () {
   'use strict';
   return false;
 };
+
+startPosition();
 
 // Slider initialization
 $('.slider').slick({
